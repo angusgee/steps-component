db7f31165b2ec48e4601c9de443855a418f24f96
MaltaWebDev <MaltaWebDev@gmail.com>
2023-12-08 14:49:01 +0100

    Refactor App component and add Steps component
    Refactor App component by extracting the Steps component from it.
    
    Introduce the StepMessage and Button components for better code organization and reusability.

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const messages = [
   "Invest your new income 🤑",
 ];
 
-function App() {
+function Steps() {
   // The useState hook must be called at the top of the component not inside an if statement or function call
   // Syntax:
   // const [stateVariable, setterFunction] = useState(initialState)
@@ -36,25 +36,49 @@ function App() {
             <div className={step >= 2 ? "active" : ""}>2</div>
             <div className={step >= 3 ? "active" : ""}>3</div>
           </div>
-          <p className="message">
-            Step {step}: {messages[step - 1]}
-          </p>
+
+          <StepMessage step={step}>{messages[step - 1]}</StepMessage>
           <div className="buttons">
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+            <Button
+              textColor={"#fff"}
+              bgColor={"#7950f2"}
               onClick={handlePrevious}
             >
-              Previous
-            </button>
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-              onClick={handleNext}
-            >
-              Next
-            </button>
+              <span>👈</span>Previous
+            </Button>
+            <Button textColor={"#fff"} bgColor={"#7950f2"} onClick={handleNext}>
+              Next<span>👉</span>
+            </Button>
           </div>
         </div>
       )}
+    </>
+  );
+}
+
+function StepMessage({ step, children }) {
+  return (
+    <div className="message">
+      <h3>Step {step}</h3> {children}
+    </div>
+  );
+}
+
+function Button({ textColor, bgColor, onClick, children }) {
+  return (
+    <button
+      style={{ backgroundColor: bgColor, color: textColor }}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
+
+function App() {
+  return (
+    <>
+      <Steps />
     </>
   );
 }
